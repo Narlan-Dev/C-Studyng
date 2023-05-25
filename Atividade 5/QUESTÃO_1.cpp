8ab8ac926140e4d6ec8df4e5c413428c964591b2
MADARINHA <n-arlan12@hotmail.com>
2023-05-24 22:45:03 -0300

    Questão 1 pronta

--- Atividade 5/QUESTÃO_1.cpp
+++ Atividade 5/QUESTÃO_1.cpp
@@ -14,13 +14,14 @@ no * busca(no *, int);
 int contaNos(no *); 
 no * inserirFim(no * , int );
 no * removerFim(no * );
+no* concatenar(no*, no*);
 
 int main(void) {
     no *lista = NULL;
 	no *lista1 = NULL; 
     int n, m, i=0, element, j;
 
-    /*cout << "Quantidade de valores? ";
+    cout << "Quantidade de valores? ";
     cin >> n;
 
 	while(i!=n){
@@ -35,7 +36,7 @@ int main(void) {
 	cout <<endl;
 	
 	while(j <= 0 || j > n){
-		cout << "Digite posição: ";
+		cout << "Digite posicao: ";
     	cin >> j;
 		cout << "\nDigite novoValor: ";
     	cin >> element;
@@ -57,7 +58,7 @@ int main(void) {
 	}
 	cout <<endl;
 	
-	cout << "Quantidades de nós: "<<contaNos(lista);*/
+	cout << "Quantidades de nohs: "<<contaNos(lista);
 
 	i = 0;
 	cout << "\nQuantidade de valores? ";
@@ -78,8 +79,12 @@ int main(void) {
 	cout << "\n Valores da lista: ";
 	imprimir(lista1);
 	cout <<endl;
+
+	cout << "\n Listas concatenadas: ";
+	imprimir(concatenar(lista, lista1));
 }
 
+//Questão 1
 no * inserirFrente(no * somelista, int valor) {
     no * temp;
     temp = new no;
@@ -94,15 +99,17 @@ no * inserirFrente(no * somelista, int valor) {
     return somelista;
 }
 
+//Questão 2
 void imprimir(no * inicio) {
     no * temp;
     temp = inicio;
     while(temp != NULL) {
-        cout << temp->element;
+        cout << "["<<temp->element<<"]";
         temp = temp->prox;
     }
 }
 
+//Questão 3
 void substituir(no * inicio, int valor, int novoValor) {
     no * temp;
 	int i;
@@ -119,6 +126,7 @@ void substituir(no * inicio, int valor, int novoValor) {
     }
 }
 
+//Questão 4
 no * removerInicio(no * inicio) {
     no * temp;
     if (inicio == NULL) {
@@ -131,6 +139,7 @@ no * removerInicio(no * inicio) {
     return inicio;
 }
 
+//Questão 5
 no * busca(no * inicio, int valor){
 	no * temp;
 	temp = inicio;
@@ -143,6 +152,7 @@ no * busca(no * inicio, int valor){
 	return NULL;
 }
 
+//Questão 6
 int contaNos(no * inicio){
 	no * temp;
 	temp = inicio;
@@ -154,6 +164,7 @@ int contaNos(no * inicio){
 	return i;
 }
 
+//Questão 7
 no * inserirFim(no * somelist, int valor){
 	no * temp, *aux;
     temp = new no;
@@ -170,19 +181,32 @@ no * inserirFim(no * somelist, int valor){
     return somelist;
 }
 
-no * removerFim(no *inicio){
-	no *temp, *aux;
-    if (inicio == NULL) {
-		cout<<"Lista vazia!";
-		return inicio;
-	}
-	aux = inicio->prox;
-	temp = inicio;
-	while(aux->prox){//Percorre até o fim da lista
-		temp = aux;
-		aux=aux->prox;
-	}
-    inicio = temp->prox;
-    delete temp;
-    return inicio;
+//Questão 8
+no * removerFim(no* inicio) {
+    if (inicio == NULL)return NULL;
+	if(inicio->prox == NULL) { //Caso lista só tenha 1 elemento
+        delete inicio;
+        return NULL;
+    } else {
+        no* aux = inicio;
+        while (aux->prox->prox != NULL)aux = aux->prox; //Percorre até o fim da lista
+        delete aux->prox;
+        aux->prox = NULL;
+        return inicio;
+    }
+}
+
+//Questão 9
+no* concatenar(no* inicio1, no* inicio2) {
+    if (inicio1 == NULL) {
+        return inicio2;
+    }
+	if(inicio2 == NULL) {
+        return inicio1;
+    }else{
+        no* aux = inicio1;
+        while (aux->prox != NULL)aux = aux->prox; //Percorre até o fim da lista
+        aux->prox = inicio2;
+        return inicio1;
+    }
 }
